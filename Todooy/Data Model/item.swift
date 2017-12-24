d1e48965b00535f71ef5d27f0b8b053fdfc11ae3
Lou Macleod <loumacleod@lous-macbook-pro.local>
2017-12-24 16:50:51 +0000

    Convert to MVC design pattern

---
+++ Todooy/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todooy
+//
+//  Created by Lou Macleod on 23/12/2017.
+//  Copyright © 2017 Lou Macleod. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
